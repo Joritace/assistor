1e01cf386cc499a3800ca18d71d449f176f2ff6b
AngomJoyceRita <joritace@gmail.com>
2026-04-12 14:54:58 +0300

    fix8

--- app/services/ml_client.py
+++ app/services/ml_client.py
@@ -12,7 +12,7 @@ def predict_from_ml_service(image_data: str) -> dict:
         response = requests.post(
             ML_SERVICE_URL,
             json={"image": image_data},
-            timeout=5
+            timeout=15
         )
 
         if response.status_code != 200:
